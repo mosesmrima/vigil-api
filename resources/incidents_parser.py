39ea7b4993f7b4c6ecc9c789dfe6a61bc5a1d64f
mrima <mrimamss@gmail.com>
2022-11-01 10:42:43 +0300

    parse incidents and geneate a json file

---
+++ resources/incidents_parser.py
@@ -0,0 +1,36 @@
+#!/isr/bin/python3
+import json
+incidents = []
+
+
+def parser(incident):
+    time, event = incident.split(".", 1)
+    month, year = time.strip().split(" ")
+
+    desc = ""
+    try:
+        desc, country = event.strip().split("#")
+    except ValueError:
+        country = ""
+        desc = event.strip()
+    incident_dict = {
+        "month": month,
+        "year": year,
+        "description": desc,
+        "country": country
+    }
+    incidents.append(incident_dict)
+
+
+with open("./incidents.txt", "r") as f:
+    incident = ""
+    for line in f:
+        if not line.isspace():
+            incident += line.replace("\n", " ")
+        else:
+            parser(incident)
+            incident = ""
+
+
+with open("./incidents.json", "w") as f:
+    json.dump(incidents, f)
