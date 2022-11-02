246f2209b49e9b25c93c5e0a97680b5d74eecf59
mrima <mrimamss@gmail.com>
2022-11-02 21:15:12 +0300

    migrate collections to atlas

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose")
 const incidentsRouter = require("./routes/incidents")
+const uri = "mongodb+srv://mrima:V60NCKzaRadUzIZ1@vigil.vdcinmp.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, dbName: "vigil" }, () => console.log("connected"))
+
+mongoose.connection.on('error', err => {
+    console.log(err);
+});
 
 const app = express();
 
